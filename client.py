7719d8dbefb14e9c8e2d6fd206f96721e14897db
Omer <barakom@post.bgu.ac.il>
2020-12-31 14:36:57 +0200

    added dynamic ip for client

--- client.py
+++ client.py
@@ -2,14 +2,17 @@ import socket
 import getch
 import struct
 import select
+from scapy.all import get_if_addr
 
-myIP = "localhost"
+myIP = get_if_addr('eth1')
 MY_PORT = 13117
-TCP_PORT = 3000
 MAGIC_COOKIE = 0xfeedbeef
 BUFFER_SIZE = 1024
 TEAM_NAME = '"DROP TABLE teamNames; --'
+tcp_port = 0
 sockUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_port.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+udp_port.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 sockUDP.bind((myIP, MY_PORT))
 print("Client started, listening for offer requests...")
 while True:
@@ -18,14 +21,15 @@ while True:
     except ConnectionResetError:
         print("unable to receive offer")
         continue
-    message_cookie, message_type, message_port = struct.unpack("!Ibh", data[0])
+    message_cookie, message_type, message_port = struct.unpack("!IbH", data[0])
     server_addr = data[1][0]
     print(f"{server_addr}, {message_port}")
     if message_cookie == MAGIC_COOKIE and message_type == 0x2:  #check offer
         print(f"Received offer from {server_addr}, {message_port} attempting to connect...​")
         try:
             sockTCP = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sockTCP.bind((myIP, TCP_PORT))
+            sockTCP.bind((myIP, tcp_port))
+            tcp_port = sockTCP.getsockname()[1]
             sockTCP.connect((server_addr, message_port))
         except socket.timeout as e:
             print(f"exception occured while trying to connect: {e}")
--- server.py
+++ server.py
@@ -4,16 +4,18 @@ import random
 import struct
 import time
 import select
+from scapy.all import get_if_addr
 
 lock = threading.Lock() #global lock, used for communicating between threads (when to start and stop the game, etc.)
 
 def send_offers(sock, port):     #for sending the offers through UDP
     CLIENT_PORT = 13117
-    SEND_UDP_TO = "localhost"
+    adds = get_if_addr('eth1').split(.)
+    SEND_UDP_TO = adds[0] + "." + adds[1] + ".255.255"
     while True:
         if lock.locked():   #continue sending until 10 seconds have passed, then the lock will lock and func will end
             return
-        sock.sendto(struct.pack("!Ibh",0xfeedbeef, 0x02, port), (SEND_UDP_TO, CLIENT_PORT))
+        sock.sendto(struct.pack("!IbH",0xfeedbeef, 0x02, port), (SEND_UDP_TO, CLIENT_PORT))
         time.sleep(1.0)
 
 def count_keystrokes(conn, addr, names1, names2, score, num_of_teams):  #the function that handle each client
@@ -57,20 +59,24 @@ def count_to_ten(): #for signaling when to start and finish the game
         lock.acquire()
         print("starting game!")
 
-my_ip = "localhost"
+my_ip = get_if_addr('eth1')
 BUFFER_SIZE = 1024
 sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-UDP_PORT = 2000
-TCP_PORT = 2002
-sock_udp.bind((my_ip, UDP_PORT))
+udp_port.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+udp_port.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+udp_port = 0
+tcp_port = 0
+sock_udp.bind((my_ip, udp_port))
+udp_port = sock_udp.getsockname()[1]
 team_names1 = []
 team_names2 = []
 score1 = [0]
 score2 = [0]
 num_of_teams = [0]
 sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock_tcp.bind((my_ip, TCP_PORT))
-print(f"Server started, listening on IP address {my_ip} port {TCP_PORT}")
+sock_tcp.bind((my_ip, tcp_port))
+tcp_port = sock_tcp.getsockname()[1]
+print(f"Server started, listening on IP address {my_ip} port {tcp_port}")
 while True:
     num_of_teams[0] = 0
     score1[0] = 0
@@ -78,7 +84,7 @@ while True:
     team_names1 = []
     team_names2 = []
     lock = threading.Lock()
-    sender = threading.Thread(target = send_offers, args = (sock_udp, TCP_PORT))
+    sender = threading.Thread(target = send_offers, args = (sock_udp, tcp_port))
     sender.start()  #start sending UDP offers
     sock_tcp.listen()
     counter = threading.Thread(target = count_to_ten, args = ())
